4ba8628ae4c0625c65bdf1eae60b79345a5ea386
Faizal <faizalamaan2@gmail.com>
2025-08-02 17:03:20 +0530

    send

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+   <div className="text-3xl font-bold underline text-blue-500">
+      Tailwind is working!
+    </div>
+  );
+}
+
+export default App;
